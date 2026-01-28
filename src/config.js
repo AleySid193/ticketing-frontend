03693ce8d7edb0cb87da68d311c5c3ffc7f13c61
Ali Ahmad <ali.ahmad@codingcops.org>
2026-01-28 12:54:03 +0500

    .

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //deployment url
-// export const BASE_URL = "https://todo-backend-production-5831.up.railway.app";
+export const BASE_URL = "https://todo-backend-production-5831.up.railway.app";
 
 //dev url
-export const BASE_URL = "http://10.10.91.32:4000";
+// export const BASE_URL = "http://10.10.91.32:4000";
